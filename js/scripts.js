21422559b210c3954c9a7b5f2e52d01f049a6038
Renee Sarley <reneesarley@gmail.com>
2018-06-15 09:01:26 -0700

    add Customer constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+function Customer(name, phone, notes) {
+  this.name = name
+  this.phone = phone
+  this.deliveryNotes = notes
+}
+
 function Pizza(size, cheese, toppings){
   this.size = size
   this.cheese = cheese
   this.toppings = toppings
-
 }
+var newCustomer = new Customer("Sam Jones", "503-888-1234", "Please call when you get here")
 var samsPizza = new Pizza("medimum", "vegan", "mushroom")
